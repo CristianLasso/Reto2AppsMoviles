9610463e9e8a512708684dd3cc3078843be20355
CristianLasso <47828389+CristianLasso@users.noreply.github.com>
2021-05-01 17:32:40 -0500

    login start listActivity

--- app/src/main/java/com/example/reto2appsmoviles/MainActivity.java
+++ app/src/main/java/com/example/reto2appsmoviles/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.reto2appsmoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.SurfaceControl;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(
+                (v) -> {
+                    String trainer = name.getText().toString();
+
+                    Intent intent = new Intent(this, ListActivity.class);
+                    startActivity(intent);
+                }
+        );
+
     }
+
 }
