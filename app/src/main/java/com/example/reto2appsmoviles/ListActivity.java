50c8528ad210c71c3dcdf676289cdd23b7902166
CristianLasso <47828389+CristianLasso@users.noreply.github.com>
2021-05-02 10:07:29 -0500

    Capture button in proccess

--- app/src/main/java/com/example/reto2appsmoviles/ListActivity.java
+++ app/src/main/java/com/example/reto2appsmoviles/ListActivity.java
@@ -3,12 +3,27 @@ package com.example.reto2appsmoviles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ListActivity extends AppCompatActivity {
+
+    private Button capture;
+    private EditText pokemonCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        capture = findViewById(R.id.capture);
+        pokemonCapture = findViewById(R.id.pokemonCapure);
+
+        capture.setOnClickListener(
+                (v) -> {
+                    String poke = pokemonCapture.getText().toString();
+                }
+        );
+
     }
 }
